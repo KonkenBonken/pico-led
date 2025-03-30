4d9542b1e7477c28e438dbe9aa7c9272633ef8ee
KonkenBonken <konrad05pettersson@gmail.com>
2025-03-30 11:52:18 +0200

    Changed to using Bun and Typescript for backend

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,8 @@
 import dgram from "dgram";
-import { Fire } from "./animations.js";
+import { Fire } from "./animations";
 
 const LED_COUNT = 180;
 const FRAME_RATE = 30;
-const buffer = new Uint8ClampedArray(LED_COUNT * 3);
 
 const client = dgram.createSocket("udp4");
 
@@ -13,6 +12,8 @@ const fire = Fire();
 
 setInterval(() => {
   const buffer = fire.next().value;
-  for (let i = 0; i < buffer.length; i++) buffer[i] *= brightness;
+  for (let i = 0; i < buffer.length; i++)
+    buffer[i] = (buffer[i] ?? 0) * brightness;
+
   client.send(buffer, 0, LED_COUNT * 3, 12345, "192.168.86.21");
 }, 1000 / FRAME_RATE);

63e98cf9629acc2f61a802d57055d3a945ad793c
KonkenBonken <konrad05pettersson@gmail.com>
2025-03-30 11:30:49 +0200

    Added brightness variable

--- server/index.js
+++ server/index.js
@@ -7,9 +7,12 @@ const buffer = new Uint8ClampedArray(LED_COUNT * 3);
 
 const client = dgram.createSocket("udp4");
 
+let brightness = 1 / 16;
+
 const fire = Fire();
 
 setInterval(() => {
   const buffer = fire.next().value;
+  for (let i = 0; i < buffer.length; i++) buffer[i] *= brightness;
   client.send(buffer, 0, LED_COUNT * 3, 12345, "192.168.86.21");
 }, 1000 / FRAME_RATE);

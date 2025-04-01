cd4d076460c00a0b67f81fd96d87433639457656
KonkenBonken <konrad05pettersson@gmail.com>
2025-04-01 23:03:26 +0200

    Added controller object and cleaned up loop and animation logic

--- server/animations.ts
+++ server/animations.ts
@@ -5,11 +5,11 @@ const LED_COUNT = 180;
 const Animations = {
   *Fire() {
     const buffer = new Uint8ClampedArray(LED_COUNT * 3);
-    const noise = new noisejs.Noise(Math.random());
+    const noise = new noisejs.Noise();
 
     while (true) {
       for (let i = 0; i < buffer.length; i += 3) {
-        const n = noise.simplex2(i / 100, Date.now() / 3e6);
+        const n = noise.simplex2(i / 100, Date.now() / 3e4);
         buffer[i + 0] = n * (101 - 1) + 1; //G
         buffer[i + 1] = n * (254 - 161) + 161; //R
         buffer[i + 2] = n * 13; //B
@@ -25,6 +25,6 @@ const Animations = {
     yield buffer;
   },
 
-} satisfies Record<string, () => Generator<Uint8ClampedArray>>;
+} satisfies Record<string, () => Generator<Uint8ClampedArray, void, never>>;
 
 export default Animations;
--- server/index.ts
+++ server/index.ts
@@ -1,25 +1,3 @@
-import dgram from "dgram";
-import Animations from "./animations";
+import controller from "./controller";
 
-const LED_COUNT = 180;
-const FRAME_RATE = 30;
-
-const client = dgram.createSocket("udp4");
-
-let brightness = 1 / 16;
-let currentAnimation: keyof typeof Animations = 'Fire';
-const generator = Animations[currentAnimation]();
-
-while (true) {
-  const frame = generator.next().value;
-  if (!frame) break;
-
-  for (let i = 0; i < frame.length; i++)
-    frame[i] = (frame[i] ?? 0) * brightness;
-
-  client.send(frame, 0, LED_COUNT * 3, 12345, "192.168.86.21");
-
-  await Bun.sleep(1000 / FRAME_RATE);
-};
-
-client.close();
+controller.startAnimation('Fire');

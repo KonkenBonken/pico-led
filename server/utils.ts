e762b3c2795d3042608c73c71754b91e91bce038
KonkenBonken <konrad05pettersson@gmail.com>
2025-08-10 22:15:43 +0200

    Added function to calculate the power consumption of a strip

--- server/utils.ts
+++ server/utils.ts
@@ -15,3 +15,18 @@ export function clamp(n: number, min: number, max: number) {
     if (min > max) [min, max] = [max, min];
     return Math.max(min, Math.min(n, max));
 }
+
+export function powerConsumption(frame: Uint8ClampedArray, white = false) {
+    const mA = {
+        R: 7.98 / 255,
+        G: 8.11 / 255,
+        B: 7.98 / 255,
+        W: 16.11 / 255,
+    };
+    let sum = 0;
+    for (let i = 0; i < frame.length; i += white ? 4 : 3) {
+        sum += frame[i + 0] * mA.R + frame[i + 1] * mA.G + frame[i + 2] * mA.B;
+        if (white) sum += frame[i + 3] * mA.W;
+    }
+    return (sum * 5) / 1000; // W
+}
